ce6cd27798be0c8eff77ec2c78cdcd5df7a0e1f7
Ryan K Burns <ryankburns@gmail.com>
2020-09-28 17:07:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
     name_hash.each do |name, value|
       values_array << value
     end
-    min_num = array[0]
+    min_num = values_array[0]
     values_array.each do |value|
       min_num = value if value < min_num
     end
